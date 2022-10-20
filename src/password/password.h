39b584d7bb43e89276e66ca78cae79a524c89756
Opixelum <48217573+opixelum@users.noreply.github.com>
2022-10-20 16:11:01 +0200

    Refactor checking functions & print errors (#7)

--- src/password/password.h
+++ src/password/password.h
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <time.h>
 
 #define SYMBOLS "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
-int pswdCheck(char *, size_t);
-char *genPswd(size_t);
+int hasLength(size_t, int);
+int hasSpecialChar(char *, size_t);
+int hasLowercase(char *, size_t);
+int hasUppercase(char *, size_t);
+int pswdCheck(char *, size_t, int, char *);
+char *genPswd(size_t, int);
