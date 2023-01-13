d459d9c01035e1a59b64023cc6548bd0934b8c41
Romain <romain.nerot@outlook.fr>
2023-01-13 15:33:48 +0100

    doxygen

--- include/credentials.h
+++ include/credentials.h
@@ -10,5 +10,9 @@ typedef struct Credentials_t {
     char *password;
 } Credentials_t;
 
-
+/**
+ * @brief Store credentials in a csv file.
+ * @param Credentials_t The credentials to store.
+ * @return 0 if successful, 1 if not.
+*/
 char storeCredentials(Credentials_t);
