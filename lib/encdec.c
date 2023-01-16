fab701c0276167c475ca8ba03d11d7462ab38b08
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-01-16 22:39:25 +0100

    Improve comments for encryption function

--- lib/encdec.c
+++ lib/encdec.c
@@ -19,21 +19,23 @@ unsigned char *generateRandomIv()
 
 ENCRYPTED_DATA_T *encrypt(char *plaintext, char *password)
 {
+    // Structure that is used to store the state of a cryptographic operation,
+    // such as encryption or decryption
     EVP_CIPHER_CTX *ctx;
+
+    // Contains the number of bytes written to the output buffer
     int len;
 
-    // Create the variable to store the encrypted data
+    // Contains the ciphertext, the iv & the tag`
     ENCRYPTED_DATA_T *encrypted_data = malloc(sizeof encrypted_data);
-
-    // Create the variable to store the ciphertext, the iv & the tag
     encrypted_data->iv = generateRandomIv();
     encrypted_data->tag = malloc(sizeof encrypted_data->tag);
     encrypted_data->ciphertext = malloc(strlen(plaintext));
 
-    /* Create and initialise the context */
+    // Create and initialise the context
     if (!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-    /* Derive the key from the password using PBKDF2 */
+    // Derive the key from the password using PBKDF2
     unsigned char key[32];
     unsigned char salt[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     int iterations = 10000;
@@ -41,21 +43,29 @@ ENCRYPTED_DATA_T *encrypt(char *plaintext, char *password)
     int salt_len = sizeof salt;
     if(1 != PKCS5_PBKDF2_HMAC(password, strlen(password), salt, salt_len, iterations, EVP_sha256(), key_len, key)) handleErrors();
 
-    /* Initialise the encryption operation */
+    // Initialize the context of the encryption operation
+    // We will set the key & the IV later, in order to separate the encryption
+    // initialization from the key and IV management
     if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) handleErrors();
+
+    // Set parameters of the cipher context & configure the behaviour of the
+    // cryptographic operation
     if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 12, NULL)) handleErrors();
+
+    // Set the key and the iv
     if (1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, encrypted_data->iv)) handleErrors();
 
-    /* Provide the message to be encrypted, and obtain the encrypted output */
+    // Provide the message to be encrypted & and obtain the encrypted output
     if (1 != EVP_EncryptUpdate(ctx, encrypted_data->ciphertext, &len, (unsigned char *) plaintext, strlen(plaintext))) handleErrors();
 
-    /* Finalise the encryption */
+    // Flush any remaining data from encryption & append any additional data to
+    // the output buffer
     if (1 != EVP_EncryptFinal_ex(ctx, encrypted_data->ciphertext + len, &len)) handleErrors();
 
-    /* Get the authentication tag */
+    // Get the authentication tag
     if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, encrypted_data->tag)) handleErrors();
 
-    /* Clean up */
+    // Clean up
     EVP_CIPHER_CTX_free(ctx);
 
     return encrypted_data;
