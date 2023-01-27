3ceaa9ee82281c41e7b512980b77bddd45d1185a
opixelum <48217573+opixelum@users.noreply.github.com>
2023-01-27 15:33:42 +0100

    Remove custom hashing functions & add WIP local account creation function

--- include/local_account.h
+++ include/local_account.h
@@ -2,19 +2,7 @@
 #include <stdio.h>
 #include "password.h"
 
-/**
- * @brief Hash a string using SHA512.
- * @param string The string to hash .
- * @return The hash of the string.
- * @warning The returned hash must be freed after use.
- */
-unsigned char *sha512Hash(const char *string);
-
-/**
- * @brief Compare two hashes using SHA512.
- * @return 0 if hashes are equal, a non-zero value otherwise.
- */
-int sha512HashCompare(const unsigned char *hash1, const unsigned char *hash2);
+#define SALT_LENGTH 255
 
 /**
  * @brief Create a local account.
@@ -24,7 +12,7 @@ int sha512HashCompare(const unsigned char *hash1, const unsigned char *hash2);
  * 
  * @param password The password to use to create the local account
  * 
- * @return 1 on success, -1 if password is not strong enough, -2 for other
+ * @return 0 on success, -1 if password is not strong enough, -2 for other
  * errors
  */
 char createLocalAccount(char *password);
@@ -32,13 +20,13 @@ char createLocalAccount(char *password);
 /**
  * @brief Connect to the local account in order to use the password manager
  * @param password The password to use to connect to the local account
- * @return 1 on success, -1 if wrong password, -2 for other errors
+ * @return 0 on success, -1 if wrong password, -2 for other errors
  */
 char connectLocalAccount(char *password);
 
 /**
  * @brief Delete the local account with all its credentials
  * @param password The password to use to delete the local account
- * @return 1 on success, -1 if wrong password, -2 for other errors
+ * @return 0 on success, -1 if wrong password, -2 for other errors
  */
 char removeLocalAccount(char *password);
--- lib/local_account.c
+++ lib/local_account.c
@@ -1,15 +1,86 @@
 #include "local_account.h"
 
-unsigned char *sha512Hash(const char *string)
+char createLocalAccount(char *password)
 {
-    unsigned char *hash = malloc(sizeof *hash * SHA512_DIGEST_LENGTH);
+    /*
+    // Check if password is strong enough
+    if (minimumPasswordRequirementsCheck(password) == 1)
+    {
+        printf("Password doesn't meet minimum requirements.\n");
+        return -1;
+    }
+*/
+    // Generate random salt
+    printf("Generating random salt... ");
+    char *salt = malloc(sizeof *salt * SALT_LENGTH + 1);
+    if (!salt)
+    {
+        fprintf(stderr, "Memory allocation for salt failed.\n");
+        return -2;
+    }
+    srand(time(NULL));
+    for (int i = 0; i < SALT_LENGTH; i++) salt[i] = 33 + rand() % 94;
 
-    SHA512((unsigned char *) string, SHA512_DIGEST_LENGTH, hash);
+    // Add null terminator to salt
+    salt[SALT_LENGTH] = '\0';
+    printf("Done.\nSalt: %s\n", salt);
 
-    return hash;
-}
+    // Reallocate password memory to concatenate salt
+    printf("Concatenating salt to password... ");
+    realloc(password, sizeof *password * (strlen(password) + SALT_LENGTH) + 1);
+    if (!password)
+    {
+        fprintf(stderr, "Memory reallocation for password failed.\n");
+        return -2;
+    }
+    strcat(password, salt);
+    printf("Done.\nPassword with hash: %s\n", password);
 
-int sha512HashCompare(const unsigned char *hash1, const unsigned char *hash2)
-{
-    return memcmp(hash1, hash2, SHA512_DIGEST_LENGTH);
+    // Hash password
+    printf("Hashing password... ");
+    unsigned char *password_hash = malloc(sizeof *password_hash * SHA512_DIGEST_LENGTH);
+    if (!password_hash)
+    {
+        fprintf(stderr, "Memory allocation for password hash failed.\n");
+        return -2;
+    }
+    SHA512((unsigned char *)password, strlen(password), password_hash);
+    printf("Done.\nPassword hash: ");
+    for (int i = 0; i < SHA512_DIGEST_LENGTH; i++) printf("%02x", password_hash[i]);
+    printf("\n");
+
+    // Open local account file
+    FILE *local_account_file = fopen("local_account", "w");
+    if (!local_account_file)
+    {
+        fprintf(stderr, "Failed to open local account file.\n");
+        return -2;
+    }
+
+    // Write salt and password hash to local account file
+    if (fwrite(salt, sizeof *salt, SALT_LENGTH, local_account_file) != SALT_LENGTH)
+    {
+        fprintf(stderr, "Failed to write salt to local account file.\n");
+        return -2;
+    }
+
+    if (fwrite(password_hash, sizeof *password_hash, SHA512_DIGEST_LENGTH, local_account_file) != SHA512_DIGEST_LENGTH)
+    {
+        fprintf(stderr, "Failed to write password hash to local account file.\n");
+        return -2;
+    }
+
+    // Close local account file
+    if (fclose(local_account_file) != 0)
+    {
+        fprintf(stderr, "Failed to close local account file.\n");
+        return -2;
+    }
+
+    // Free memory
+    free(salt);
+    free(password_hash);
+    free(password);
+
+    return 0;
 }
