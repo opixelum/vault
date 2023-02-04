d1b903e1fa9edb8a828427b88cb8c81d12227c9a
opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-04 01:09:18 +0100

    Remove todo comment

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "ui.h"
 #include <unistd.h>
 
-// TODO: On launch, check if there is a local account. If yes, ask for password, if no, ask for password creation
-
 int main(int argc, char const *argv[])
 {
     unsigned char isRunning = 1;
