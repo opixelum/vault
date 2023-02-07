6d090524f8ff1c674c9c541952e4750044b3b640
Romain <romain.nerot@veolia.com>
2023-02-07 16:39:12 +0100

    Write on different pages

--- lib/export.c
+++ lib/export.c
@@ -38,54 +38,75 @@ char exportCredentials()
         return 1;
     }
 
-    /* Create a new page */
-    page = HPDF_AddPage(pdf);
-    HPDF_Page_SetSize(page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
-
-    /* Set the font and font size */
-    font = HPDF_GetFont(pdf, "Helvetica", NULL);
-    HPDF_Page_SetFontAndSize(page, font, 12);
-
     // Read the header line
     fgets(line, MAX_LENGTH, file);
 
     int y = 750;
     int x = 50;
 
-    /* Write the content of the credentials variable to the page */
-    while (fgets(line, MAX_LENGTH, file) != NULL)
+    const int THRESHOLD = 60;
+    const int START_Y = y;
+
+    while (fgets(line, MAX_LENGTH, file))
     {
+        if (y == START_Y)
+        {
+            /* Create a new page */
+            page = HPDF_AddPage(pdf);
+            HPDF_Page_SetSize(page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
+
+            /* Set the font and font size */
+            font = HPDF_GetFont(pdf, "Helvetica", NULL);
+            HPDF_Page_SetFontAndSize(page, font, 12);
+        }
+
+        /* Write the content of the credentials variable to the page */
         sscanf(line, "%[^,],%[^,],%[^,],%[^,],%[^\n]", label, url, username, email, password);
+
         HPDF_Page_BeginText(page);
         HPDF_Page_MoveTextPos(page, x, y);
         HPDF_Page_ShowText(page, "Label: ");
         HPDF_Page_ShowText(page, label);
         HPDF_Page_EndText(page);
+
         y -= 20;
+
         HPDF_Page_BeginText(page);
         HPDF_Page_MoveTextPos(page, x, y);
         HPDF_Page_ShowText(page, "Url: ");
         HPDF_Page_ShowText(page, url);
         HPDF_Page_EndText(page);
+
         y -= 20;
+
         HPDF_Page_BeginText(page);
         HPDF_Page_MoveTextPos(page, x, y);
         HPDF_Page_ShowText(page, "Username: ");
         HPDF_Page_ShowText(page, username);
         HPDF_Page_EndText(page);
+
         y -= 20;
+
         HPDF_Page_BeginText(page);
         HPDF_Page_MoveTextPos(page, x, y);
         HPDF_Page_ShowText(page, "Email: ");
         HPDF_Page_ShowText(page, email);
         HPDF_Page_EndText(page);
+
         y -= 20;
+
         HPDF_Page_BeginText(page);
         HPDF_Page_MoveTextPos(page, x, y);
         HPDF_Page_ShowText(page, "Password: ");
         HPDF_Page_ShowText(page, password);
         HPDF_Page_EndText(page);
+
         y -= 40;
+
+        if (y < THRESHOLD)
+        {
+            y = START_Y;
+        }
     }
 
     /* Save the PDF file */
