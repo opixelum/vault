6fe5b62aa70cc3f1bbcd7e0d5b82db8cef99fe6c
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-08 10:45:16 +0100

    Create local account (#29)
    * Add local account library
    
    * Add minimum password requirement check
    
    * Add libs linking
    
    * Add simpler sha512 function
    
    * Add simpler hash compare function
    
    * Ignore local account file
    
    * Add null terminator to generated passwords
    
    * Remove custom hashing functions & add WIP local account creation function
    
    * Add test for local cacount creation
    
    * Fix realloc & separate hash & hash with newline in local account file
    
    * WIP: hashes are the same
    
    * WIP: password always matches with binaries
    
    * Password matches with strings
    
    * Refactor
    
    * Improve local account test
    
    * Improve password check & check if password meets minimum requirements
    
    * Check if local account exists
    
    * Implement deleteLocalAccount()
    
    * Add ui dialogue for local account creation
    
    * Add localAccountLogInDialogue()
    
    * Remove useless comment
    
    * Add getCharHide function
    
    * Get local account password without displaying it
    
    * Fix typo
    
    * Remove todo comment
    
    * Get string without displaying it
    
    * Add all local account dialogue functions declarations
    
    * Fix deleting characters in getStringHide
    
    * Add main menu choice for managing local account
    
    * Hide local account password at creation
    
    * WIP: fix trailing \n after menu choices & change local account password
    
    * Change local account password
    
    * Add delete local account dialogue
    
    * Add 'hex' in password hash from file variable
    
    * Add comments to getCharHide
    
    * Rename 'c' variable to 'character'
    
    * Remove del because it can't be use in our case for the moment
    
    * Rename 'c' variable to 'character'
    
    * Remove useless blankline
    
    * Remove useless header file

--- include/ui.h
+++ include/ui.h
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include "password.h"
 #include "credentials.h"
+#include "local_account.h"
+#include <termios.h>
+#include <unistd.h>
 #include "export.h"
+
+/**
+ * @brief Get a character from stdin without displaying it.
+ * @return The character read from stdin.
+ */
+unsigned char getCharHide();
+
+/**
+ * @brief Get a string from stdin without displaying it.
+ * @return The string read from stdin.
+*/
+char *getStringHide();
+
+/**
+ * @brief Clear the stdin buffer.
+ * Useful for clearing the buffer before or after a scanf.
+ */
+void clearStdin();
+
+/**
+ * @brief Display the dialogue for creating a new local account.
+ */
+void localAccountCreationDialogue();
+
+/**
+ * @brief Display the dialogue for logging in to a local account.
+ */
+void localAccountLogInDialogue();
 
 /**
  * @brief Display main menu where user can choose which functionality to run.
  */
-void mainMenu(unsigned char *isRunning);
+void mainMenu(unsigned char *isRunning, unsigned char *isConnected);
 
 /**
  * @brief Wait for user to press enter. Used to pause the program.
@@ -17,3 +48,18 @@ void pressEnterToContinue();
  * @brief Display the dialogue for creating new credentials.
  */
 void createCredentialsDialogue();
+
+/**
+ * @brief Display the dialogue for managing local account.
+ */
+void manageLocalAccountMenu();
+
+/**
+ * @brief Display the dialogue for changing the password of the local account.
+ */
+void changeLocalAccountPasswordDialogue();
+
+/**
+ * @brief Display the dialogue for deleting the local account.
+ */
+void deleteLocalAccountDialogue(unsigned char *isRunning, unsigned char *isConnected);
