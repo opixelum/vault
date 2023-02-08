4ea70a610cf3f794f75e1c521b7f727025e55de4
Romain <romain.nerot@veolia.com>
2023-02-08 15:17:08 +0100

    Export data as CSV

--- lib/export.c
+++ lib/export.c
@@ -156,5 +156,66 @@ char exportCredentialsAsPDF()
 
 char exportCredentialsAsCSV()
 {
+    char *line = malloc(MAX_LENGTH * sizeof(char));
+    char *label = malloc(MAX_LENGTH * sizeof(char));
+    char *url = malloc(MAX_LENGTH * sizeof(char));
+    char *username = malloc(MAX_LENGTH * sizeof(char));
+    char *email = malloc(MAX_LENGTH * sizeof(char));
+    char *password = malloc(MAX_LENGTH * sizeof(char));
+
+    // Get the credentials from the user and store them in a variable
+    FILE *file = fopen("credentials.csv", "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Could not open file.\n");
+        return -1;
+    }
+
+    time_t current_time = time(NULL);
+    struct tm *local_time = localtime(&current_time);
+
+    char file_name[100];
+    sprintf(file_name, "exported_credentials_%02d-%02d-%04d_%02d-%02d-%02d.csv", local_time->tm_mday, local_time->tm_mon + 1, local_time->tm_year + 1900, local_time->tm_hour, local_time->tm_min, local_time->tm_sec);
+
+    FILE *exported_file = fopen(file_name, "w");
+    if (exported_file == NULL)
+    {
+        fprintf(stderr, "Error: Could not open file.\n");
+        return -1;
+    }
+
+    // Read the header line
+    fgets(line, MAX_LENGTH, file);
+    fprintf(exported_file, "Label, URL, Username, Email, Password\n");
+
+    // Write data into new file
+    while (fgets(line, MAX_LENGTH, file))
+    {
+        sscanf(line, "%[^,],%[^,],%[^,],%[^,],%[^\n]", label, url, username, email, password);
+
+        char *csv_text = strcat(label, ",");
+        csv_text = strcat(csv_text, url);
+        csv_text = strcat(csv_text, ",");
+        csv_text = strcat(csv_text, username);
+        csv_text = strcat(csv_text, ",");
+        csv_text = strcat(csv_text, email);
+        csv_text = strcat(csv_text, ",");
+        csv_text = strcat(csv_text, password);
+        csv_text = strcat(csv_text, "\n");
+
+        if (fprintf(exported_file, "%s", csv_text) == EOF)
+        {
+            fprintf(stderr, "Error: Could not write to file.\n");
+            return -1;
+        }
+    }
+
+    // Close file
+    if (fclose(file) == EOF)
+    {
+        fprintf(stderr, "Error: Could not close file.\n");
+        return -1;
+    }
+
     return 0;
 }
