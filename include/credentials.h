c1d496134721c170718f5cef29a7b0df6ca13f99
opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-15 20:07:38 -0400

    Store first credentials in encrypted file

--- include/credentials.h
+++ include/credentials.h
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "password.h"
+#include "encdec.h"
+#include "utils.h"
 
 typedef struct CredentialsStruct
 {
@@ -13,9 +16,11 @@ typedef struct CredentialsStruct
 }
 CREDENTIALS_T;
 
+char storeFirstCredentials(CREDENTIALS_T credentials, char * password);
+
 /**
  * @brief Store credentials in a csv file.
  * @param credentials The credentials to store.
  * @return 0 if successful, 1 if not.
 */
-char storeCredentials(CREDENTIALS_T credentials);
+char storeCredentials(CREDENTIALS_T credentials, char * password);
--- lib/credentials.c
+++ lib/credentials.c
@@ -1,47 +1,131 @@
 #include "credentials.h"
 
-char storeCredentials(CREDENTIALS_T credentials)
+char storeFirstCredentials(CREDENTIALS_T credentials, char * password)
 {
-    // Open file
-    FILE *file = fopen("credentials.csv", "r");
-    if (file == NULL)
-    {
-        file = fopen("credentials.csv", "a");
-        fprintf(file, "Label, URL, Username, Email, Password\n");
-    }
-    else
-    {
-        file = fopen("credentials.csv", "a");
-        if (file == NULL)
-        {
-            fprintf(stderr, "Error: Could not open file.\n");
-            return -1;
-        }
-    }
+    // Create data folder if it doesn't exist
+    if (access("data", F_OK) == -1) mkdir("data", 0700);
 
-    // Write to file
-    char *csv_text = strcat(credentials.label, ",");
-    csv_text = strcat(csv_text, credentials.url);
-    csv_text = strcat(csv_text, ",");
-    csv_text = strcat(csv_text, credentials.username);
-    csv_text = strcat(csv_text, ",");
-    csv_text = strcat(csv_text, credentials.email);
-    csv_text = strcat(csv_text, ",");
-    csv_text = strcat(csv_text, credentials.password);
-    csv_text = strcat(csv_text, "\n");
-
-    if (fprintf(file, "%s", csv_text) == EOF)
+    // Format credentials in CSV
+    char * credentials_csv_header = "label,url,username,email,password\n";
+    char * credentials_csv_row = calloc
+    (
+        strlen(credentials.label) +
+        strlen(credentials.url) +
+        strlen(credentials.username) +
+        strlen(credentials.email) +
+        strlen(credentials.password) +
+        6, // 4 commas + 1 newline + 1 null terminator
+        sizeof * credentials_csv_row
+    );
+    if (!credentials_csv_row)
     {
-        fprintf(stderr, "Error: Could not write to file.\n");
+        fprintf(stderr, "ERROR: `credentials_csv_row` calloc failed.\n");
         return -1;
     }
-
-    // Close file
-    if (fclose(file) == EOF)
+    sprintf
+    (
+        credentials_csv_row,
+        "%s,%s,%s,%s,%s\n",
+        credentials.label,
+        credentials.url,
+        credentials.username,
+        credentials.email,
+        credentials.password
+    );
+    credentials_csv_row[strlen(credentials_csv_row)] = '\0';
+    char * credentials_csv_file_content = calloc
+    (
+        strlen(credentials_csv_header) + strlen(credentials_csv_row) + 1,
+        sizeof * credentials_csv_file_content
+    );
+    if (!credentials_csv_file_content)
     {
-        fprintf(stderr, "Error: Could not close file.\n");
+        fprintf(stderr, "ERROR: `credentials_csv_file_content` calloc failed.\n");
         return -1;
     }
+    strcpy(credentials_csv_file_content, credentials_csv_header);
+    strcat(credentials_csv_file_content, credentials_csv_row);
+    free(credentials_csv_row);
+
+    // Encrypt credentials CSV file content
+    ENCRYPTED_DATA_T * encrypted_credentials_csv_file_content = encrypt
+    (
+        credentials_csv_file_content,
+        password
+    );
+
+    // Store IV
+    char * iv_file_path = getEncDecFilePath("iv");
+    FILE * iv_file = fopen(iv_file_path, "wb");
+    fwrite
+    (
+        encrypted_credentials_csv_file_content->iv,
+        sizeof encrypted_credentials_csv_file_content->iv,
+        IV_LENGTH,
+        iv_file
+    );
+    free(encrypted_credentials_csv_file_content->iv);
+    free(iv_file_path);
+    fclose(iv_file);
+
+    // Store tag
+    char * tag_file_path = getEncDecFilePath("tag");
+    FILE * tag_file = fopen(tag_file_path, "wb");
+    fwrite
+    (
+        encrypted_credentials_csv_file_content->tag,
+        sizeof encrypted_credentials_csv_file_content->tag,
+        TAG_LENGTH,
+        tag_file
+    );
+    free(encrypted_credentials_csv_file_content->tag);
+    free(tag_file_path);
+    fclose(tag_file);
+
+    // Store encrypted credentials CSV file content
+    char * encrypted_credentials_csv_file_path = getEncDecFilePath("credentials");
+    FILE * encrypted_credentials_file = fopen(encrypted_credentials_csv_file_path, "wb");
+    fwrite
+    (
+        encrypted_credentials_csv_file_content->ciphertext,
+        sizeof encrypted_credentials_csv_file_content->ciphertext,
+        strlen((char *)encrypted_credentials_csv_file_content->ciphertext),
+        encrypted_credentials_file
+    );
+    free(encrypted_credentials_csv_file_content->ciphertext);
+    free(encrypted_credentials_csv_file_content);
+    free(encrypted_credentials_csv_file_path);
+    fclose(encrypted_credentials_file);
+
+    return 0;
+}
+
+char storeCredentials(CREDENTIALS_T credentials, char * password)
+{
+    FILE *encrypted_credentials_file = NULL;
+
+    // Get encrypted credentials file path
+    char *encrypted_credentials_file_path = getEncDecFilePath("credentials");
+
+    // Check if encrypted credentials file exists
+    if (access(encrypted_credentials_file_path, F_OK) != -1)
+    {
+        // Create data folder if it doesn't exist
+        if (access("data", F_OK) == -1) mkdir("data", 0700);
+
+        // Create encrypted credentials file
+        encrypted_credentials_file = fopen(encrypted_credentials_file_path, "w");
+        fclose(encrypted_credentials_file);
+    }
+
+    // Open encrypted credentials file
+    encrypted_credentials_file = fopen(encrypted_credentials_file_path, "rb");
+
+    // Get encrypted credentials file size
+    size_t encrypted_credentials_file_size = getFileSize(encrypted_credentials_file);
+
+    // Get encrypted credentials file content
+    unsigned char *encrypted_credentials_file_content = calloc(encrypted_credentials_file_size, sizeof *encrypted_credentials_file_content);
 
     return 0;
 }
