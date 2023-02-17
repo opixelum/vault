e040c4c6b4bd191d286325482300c666712fcdc9
opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-17 18:28:43 -0400

    Delete unused utils library

--- include/utils.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * @brief Get the file size
- * @param file A file pointer
- * @return The file size as a unsigned long or size_t
- */
-size_t getFileSize(FILE * file);
