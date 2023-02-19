c25c612db2beee792c45a659ce575a58e064bdc6
opixelum <48217573+opixelum@users.noreply.github.com>
2023-02-19 12:42:51 -0400

    Remove utils file because deprecated

--- include/utils.h
+++
@@ -1,8 +0,0 @@
-#include <sys/stat.h>
-
-/**
- * @brief Check if given path is a directory.
- * @param path Path to check.
- * @return 1 if path is a directory, 0 otherwise.
- */
-unsigned char isDirectory(const char * path);
